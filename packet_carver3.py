d52f886e826786c3b3d2d4c799c58ca57956ceda
Grant Knoetze <69013855+Grant-Knoetze@users.noreply.github.com>
2022-08-13 12:31:39 +0200

    Add plugin requirements.
    Add netscan and netstat plugin requirements.

--- packet_carver3.py
+++ packet_carver3.py
@@ -1,7 +1,41 @@
 #!/usr/bin/env python3
 
-from volatility.framework import interfaces
+# Import modules
+import binascii
+import datetime
+import json
+import os
+import re
+import socket
+import sys
+
+from volatility3.framework import interfaces
+from volatility3.plugins.windows import *
+from volatility3.framework.configuration import requirements
+
 
 # Define a class that inherits from PluginInterface
-class packet_carver3(interfaces.plugins.PluginInterface):
-    pass 
+class PacketCarver(interfaces.plugins.PluginInterface):
+
+    @classmethod
+    def get_requirements(cls):
+        return [requirements.TranslationLayerRequirement(name='primary',
+                                                         description='Memory layer for the kernel',
+                                                         architectures=["Intel32", "Intel64"]),
+                requirements.SymbolTableRequirement(name="nt_symbols",
+                                                    description="Windows kernel symbols"),
+                requirements.PluginRequirement(name='pslist',
+                                               plugin=pslist.PsList,
+                                               version=(1, 0, 0)),
+                requirements.PluginRequirement(name='netstat',
+                                               plugin=netstat.NetStat,
+                                               version=(1, 0, 0)),
+                requirements.PluginRequirement(name='netstat',
+                                               plugin=netscan.NetScan,
+                                               version=(1, 0, 0)),
+                requirements.ListRequirement(name='pid',
+                                             element_type=int,
+                                             description="Process IDs to include (all other processes are excluded)",
+                                             optional=True)]
+
+# Define the plugin requirements.
