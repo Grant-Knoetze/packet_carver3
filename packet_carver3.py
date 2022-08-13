7e83e8517ace0ebf04fea617cb8fbe3ddc1694ad
Grant Knoetze <69013855+Grant-Knoetze@users.noreply.github.com>
2022-08-13 10:34:21 +0200

    Add packet_carver3 class.
    Add packet_carver3 class to inherit from PluginInterface.
    Import interfaces from Volatility.

---
+++ packet_carver3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+from volatility.framework import interfaces
+
+# Define a class that inherits from PluginInterface
+class packet_carver3(interfaces.plugins.PluginInterface):
+    pass 
