7dfd9e0bfcddbfe04ccf884da1125759a651d40a
Grant Knoetze <69013855+Grant-Knoetze@users.noreply.github.com>
2022-10-28 13:48:27 +0200

    Add ip's not to test function.

--- packet_carver3.py
+++ packet_carver3.py
@@ -99,6 +99,37 @@ class PacketCarver(interfaces.plugins.PluginInterface):
         ipv4 = re.compile(r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$')
         return ipv4.match(ip)
 
+    @classmethod
+    def ips_not_to_test(cls, ip):
+        """
+        Function that tests to see if ip's are worth testing, ie: not loopback, internal, bogon etc
+        """
+        # 10.0.0.0 - 10.255.255.255
+        if ip[:3] == '10.':
+            return True
+
+        # 172.16.0.0 - 172.31.255.255
+        if ip[:4] == '172.' and ip[6:7] == '.' and int(ip[4:6]) in range(16, 31, 1):
+            return True
+
+        # 192.168.0.0 - 192.168.255.255
+        if ip[:8] == '192.168.':
+            return True
+
+        # 255.255.255.255
+        if ip == '255.255.255.255':
+            return True
+
+        # Multicast 224.0.0.0 - 239.255.255.255
+        if int(ip[:3]) in range(224, 240, 1):
+            return True
+
+        # 0.0.0.0
+        if ip == '0.0.0.0':
+            return True
+
+        return False
+
 
 # We should figure out what we are returning
 
