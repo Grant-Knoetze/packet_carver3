5fa359fdb50eec02f2dc4fd9b0c2e465cd5dc455
Grant Knoetze <69013855+Grant-Knoetze@users.noreply.github.com>
2022-10-28 13:27:32 +0200

    Add rawstring r to regex.

--- packet_carver3.py
+++ packet_carver3.py
@@ -96,7 +96,7 @@ class PacketCarver(interfaces.plugins.PluginInterface):
         """
         Check IP address to confirm if IPV4
         """
-        ipv4 = re.compile('^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$')
+        ipv4 = re.compile(r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$')
         return ipv4.match(ip)
 
 
